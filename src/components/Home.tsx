72d3b1ac1a92dc3e32ee8a50b77d1ad775ac2391
Vivekanand P V <vivek@anviksha.io>
2024-07-19 22:14:00 +0530

    done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface HomeProps {}
 
 const Home = (props: HomeProps) => {
+  const navigator = useNavigate();
+
+  const toTechNews = () => {
+    navigator('/news/tech', {
+      state: {
+        message: 'message passed by programmatic navigation from Home',
+      },
+    });
+  };
+
   return (
     <>
       <h4>Home Component</h4>
+      <button className='btn btn-primary' onClick={toTechNews}>
+        To Tech News
+      </button>
     </>
   );
 };
